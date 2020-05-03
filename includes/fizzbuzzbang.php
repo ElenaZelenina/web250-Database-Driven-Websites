b0bc699cd00b0415ab6cff368003ef8aed1e3cef
Elena Zelenina <zelenina.elena@gmail.com>
2020-05-03 17:36:49 -0400

    Update home and FizBuzz files

--- includes/fizzbuzzbang.php
+++ includes/fizzbuzzbang.php
@@ -2,66 +2,72 @@
 
 <form action="#" method="post">
     <fieldset>
-        <legend>POST Form</legend>
-        <div><label for="postname">Please enter your name:</label>
-            <input type="text" id="postname" name="name" value="John Doe" size="100"></div>
-        <div><label for="postlname">Last name:</label>
-            <input type="text" id="postlname" name="lname" value="Doe" size="50"></div>
-        <p>Favorite character:</p>
-        <div>
-            <input type="checkbox" id="postcharacter1" name="character1" value="Bean">
-            <label for="postcharacter1"> I like Bean</label>
-        </div>
-        <div>
-            <input type="checkbox" id ="postcharacter2" name="character2" value="Elfo">
-            <label for="postcharacter2"> I like Elfo</label>
-        </div>
-        <div>
-            <input type="checkbox" id="postcharacter3" name="character3" value="Luci">
-            <label for="postcharacter3"> I like Luci</label>
-        </div>
+        <div><label for="postname">Your name:</label>
+            <input type="text" id="postname" name="name" value="John Doe" size="40"></div>
+        <div><label for="startnumber">Count from:</label>
+            <input type="number" id="startnumber" name="startnumber" value="1"></div>
+        <div><label for="endnumber">Count to:</label>
+            <input type="number" id="endnumber" name="endnumber" value="100"></div>
+        <div>Enter the values to be used</div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Word to display</th>
+                    <th>Number</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td> <input type="text" id="word1" name="word1" value="Fizz"></td>
+                    <td><input type="number" id="number1" name="number1" value="3"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" id="word2" name="word2" value="Buzz"></td>
+                    <td><input type="number" id="number2" name="number2" value="5"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" id="word3" name="word3" value="Bang"></td>
+                    <td><input type="number" id="number3" name="number3" value="7"></td>
+                </tr>
+            </tbody>
+        </table>
         <input type="submit" value="Submit">
     </fieldset>
 </form>
-
-
 <?php
 
-if (isset($_POST["fname"])) {
-
-    if (!isset($_POST["character1"]) && !isset($_POST["character2"]) && !isset($_POST["character3"])) {
-        $ouputString2 = "Did you ever watch the show?";
-    } else {
-        $ouputString2 = "";
-        if (isset($_POST["character1"])) {
-            $ouputString2 .= $_POST["character1"];
-        }
-        if (isset($_POST["character2"])) {
-            $ouputString2 .= ($ouputString2 == "" ? "" : " and ") . $_POST["character2"];
-        }
-        if (isset($_POST["character3"])) {
-            $ouputString2 .= ($ouputString2 == "" ? "" : " and ") . $_POST["character3"];
-        }
-        $ouputString2 = "You like " . $ouputString2 . "!";
-    }
-    $ouputString1 = "Welcome, " . $_POST["fname"] . " " . $_POST["lname"] . "!<br>";
+if (isset($_POST["name"])) {
+    $ouputString1 = "Welcome, " . $_POST["name"] . "!<br>";
     echo $ouputString1;
-    echo $ouputString2;
+    printNumber1($_POST["startnumber"], $_POST["endnumber"]);
 }
 
-printNumber1(100);
-
-function printNumber1($count)
+function printNumber1($x, $count)
 {
-    for ($x = 1; $x <= $count; $x++) {
-        if (($x % 3 == 0) && ($x % 5 == 0)) {
-            $num =  "FizzBuzz";
-        } elseif ($x % 3 == 0) {
-            $num = "Fizz";
-        } elseif ($x % 5 == 0) {
-            $num =  "Buzz";
-        } else $num = "$x";
-        echo "$num<br>";
-    }
+    $word1 = $_POST["word1"];
+    $word2 = $_POST["word2"];
+    $word3 = $_POST["word3"];
+    $number1 = $_POST["number1"];
+    $number2 = $_POST["number2"];
+    $number3 = $_POST["number3"];
+    if ($number1 != 0 && $number2 != 0 && $number3 != 0) {
+        for (; $x <= $count; $x++) {
+            $num = "";
+            if ($x % $number1 == 0) {
+                $num = $word1;
+            }
+            if ($x % $number2 == 0) {
+                $num .= $word2;
+            }
+            if ($x % $number3 == 0) {
+                $num .= $word3;
+            }
+            if ($num == "") {
+                $num = "$x";
+            }
+            echo "$num<br>";
+        }
+    } else echo "Number cannot be zero or empty.";
 }
 ?>
--- includes/home.php
+++ includes/home.php
@@ -5,7 +5,7 @@
 <li><a href="?p=forms.php">forms</a></li>
 <li><a href="?p=fizzbuzz1.php">fizz buzz 1</a></li>
 <li><a href="?p=textdriven.php">text-driven website</a></li>
-<li><a href="?p=charlie.php">charlie</a></li>
+<li><a href="?p=fizzbuzzbang.php">fizz buzz bang everything</a></li>
 <li><a href="?p=delta.php">delta</a></li>
 <li><a href="?p=echo.php">echo</a></li>
 </ul>
